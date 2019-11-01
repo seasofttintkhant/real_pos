bbaac99b573524cbbccf36bc66ecc72d660f2ee8
Tint Aung Khant <khant.a.tint@seasoft.asia>
2019-11-01 23:22:21 +0630

    admin seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::create([
+            "name" => "admin",
+            "email" => "admin@email.com",
+            "password" => bcrypt("password"),
+            "role" => "admin"
+        ]);
+    }
+}
